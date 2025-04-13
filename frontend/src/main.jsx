281685e97c4d81667f3cb884d299905b9a845f64
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2025-04-13 11:45:51 +0600

    banner section completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routers/router.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
