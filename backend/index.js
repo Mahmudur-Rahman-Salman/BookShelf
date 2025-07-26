3e3cce699338ddb971b11c59c50ef6c685413326
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2025-07-27 03:03:42 +0600

    backend post created

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require("dotenv").config();
 
+//middlewares
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173/", // Adjust this to your frontend URL
+    // methods: "GET,POST,PUT,PATCH,DELETE",
+    credentials: true,
+  })
+);
 
 // routes
+const bookRoutes = require("./src/books/book.route");
+
+app.use("/api/books", bookRoutes);
 
 async function main() {
-  await mongoose.connect(
-    process.env.DB_URL,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+  await mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   app.use("/", (req, res) => {
     res.send("BookShelf server is running!");
